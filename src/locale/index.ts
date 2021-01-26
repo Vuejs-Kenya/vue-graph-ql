642e71951bb815f7816dfcbdac201e089f2f9a76
Odhiambo Dormnic <ayimdomnic@outlook.com>
2021-01-26 15:17:57 +0300

    initialize graphQL

---
+++ src/locale/index.ts
@@ -0,0 +1,9 @@
+import VueI18n from "vue-i18n";
+
+import en from './lang/en'
+
+const messages = {
+    en
+}
+
+export default messages
---
+++ src/locale/lang/en.ts
@@ -0,0 +1,9 @@
+const en = {
+    common: {
+        title: "Vue GraphQL",
+        description: "A Simple Vue Client for Testing GraphQL",
+        author: "Odhiambo Dormnic"
+    }
+}
+
+export default en;
